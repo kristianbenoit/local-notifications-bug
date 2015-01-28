8422c60e75ff777d73f77e61783383a80d8e7fc3
Kristian Benoit <kristian.benoit@gmail.com>
2015-01-28 16:46:50 -0500

    Log stuff to see what is happening.

--- www/js/app.js
+++ www/js/app.js
@@ -72,8 +72,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
       views: {
         'tab-friends': {
           templateUrl: 'templates/tab-friends.html',
-          controller: 'FriendsCtrl'
-        }
+          controller: 'FriendsCtrl' }
       }
     })
     .state('tab.friend-detail', {
@@ -97,6 +96,68 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise(function($injector) {
+    console.log("othewise : called");
+    var tab = "dash";
+    $injector.invoke(function(clickState) {
+      if (clickState.clicked()) {
+        console.log("otherwise : got clicked, going to friends.");
+        tab = "friends"
+      }
+    });
+    return '/tab/' + tab;
+  });
+
+})
+
+.factory("clickState", function($ionicPlatform, $state, $window) {
+  console.log("clickState : initializing");
+  var gotClicked = false
+  var isReady = false;
+
+  $ionicPlatform.ready(function() {
+    console.log("clickState : Platform is ready");
+
+    function addNotif() {
+      var now = new Date();
+      now.setSeconds(now.getSeconds() + 10);
+      var friendId = Math.floor(Math.random * 5);
+
+      $window.plugin.notification.local.add({
+        id: now.getTime().toString(),
+        title: "Friend notif",
+        autoCancel: true,
+        date: now,
+        json: {friendId : friendId}
+      });
+    }
+
+    addNotif();
+    $window.plugin.notification.local.onclick = function(id, state, json) {
+      addNotif();
+      console.log("clickState: got clicked : state is '" + state + "'");
+      if (isReady) {
+        console.log("clickState : got clicked : going to tab friends");
+        $state.go("tab.friends");
+      } else {
+        console.log("clickState : got cliecked : Setting the state to clicked.");
+        gotClicked = true;
+      }
+    };
+
+  });
+
+  return {
+    clicked : function() {
+      console.log("clickeState : The system is now ready as it checks for a click");
+      isReady = true;
+      var tmp = gotClicked;
+      gotClicked = false;
+      return tmp;
+    }
+  };
 
 });
+
+document.addEventListener("pause", function() { console.log("pause"); }, false);
+document.addEventListener("resume", function() { console.log("resume"); }, false);
